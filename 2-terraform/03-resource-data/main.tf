d4483a48d78cb05d166a2eae80c8806dd0762e56
Byungjin Park <posquit0.bj@gmail.com>
2021-10-10 00:45:36 +0900

    Add 03-resource-data terraform code

---
+++ 2-terraform/03-resource-data/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.image_id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "fastcampus-ubuntu"
+  }
+}
