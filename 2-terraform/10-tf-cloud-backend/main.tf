505c244c139957c5b4b9ba6c32b1e25f4a0b8e16
Byungjin Park <posquit0.bj@gmail.com>
2021-10-11 21:54:32 +0900

    Add 10-tf-cloud-backend terraform code

---
+++ 2-terraform/10-tf-cloud-backend/main.tf
@@ -0,0 +1,100 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "fastcampus-devops"
+
+    workspaces {
+      name = "tf-cloud-backend"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+/*
+ * Groups
+ */
+
+resource "aws_iam_group" "developer" {
+  name = "developer"
+}
+
+resource "aws_iam_group" "employee" {
+  name = "employee"
+}
+
+output "groups" {
+  value = [
+    aws_iam_group.developer,
+    aws_iam_group.employee,
+  ]
+}
+
+
+/*
+ * Users
+ */
+
+variable "users" {
+  type = list(any)
+}
+
+resource "aws_iam_user" "this" {
+  for_each = {
+    for user in var.users :
+    user.name => user
+  }
+
+  name = each.key
+
+  tags = {
+    level = each.value.level
+    role  = each.value.role
+  }
+}
+
+resource "aws_iam_user_group_membership" "this" {
+  for_each = {
+    for user in var.users :
+    user.name => user
+  }
+
+  user   = each.key
+  groups = each.value.is_developer ? [aws_iam_group.developer.name, aws_iam_group.employee.name] : [aws_iam_group.employee.name]
+}
+
+locals {
+  developers = [
+    for user in var.users :
+    user
+    if user.is_developer
+  ]
+}
+
+resource "aws_iam_user_policy_attachment" "developer" {
+  for_each = {
+    for user in local.developers :
+    user.name => user
+  }
+
+  user       = each.key
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+
+  depends_on = [
+    aws_iam_user.this
+  ]
+}
+
+output "developers" {
+  value = local.developers
+}
+
+output "high_level_users" {
+  value = [
+    for user in var.users :
+    user
+    if user.level > 5
+  ]
+}
