ef4a2c9b3eb1fa5e2c1de8ab2cbcaa278dcc50e1
Byungjin Park <posquit0.bj@gmail.com>
2021-11-15 00:48:27 +0900

    Fix ubuntu install-docker script

--- 3-docker-kubernetes/env/ubuntu/install-docker.sh
+++ 3-docker-kubernetes/env/ubuntu/install-docker.sh
@@ -6,7 +6,7 @@ set -euf -o pipefail
 DOCKER_USER=ubuntu
 
 # Install dependencies
-sudo apt-get update && apt-get install -y \
+sudo apt-get update && sudo apt-get install -y \
   apt-transport-https \
   ca-certificates \
   curl \
