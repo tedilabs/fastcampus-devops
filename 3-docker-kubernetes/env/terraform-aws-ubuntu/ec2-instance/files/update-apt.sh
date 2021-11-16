6433dc454fb27e46348641b13171a9349f7d042c
Byungjin Park <posquit0.bj@gmail.com>
2021-11-17 00:07:51 +0900

    Fix update-apt.sh for terraform-aws-ubuntu

--- 3-docker-kubernetes/env/terraform-aws-ubuntu/ec2-instance/files/update-apt.sh
+++ 3-docker-kubernetes/env/terraform-aws-ubuntu/ec2-instance/files/update-apt.sh
@@ -5,6 +5,9 @@ set -euf -o pipefail
 # Set default answer for any questions a package might ask
 export DEBIAN_FRONTEND=noninteractive
 
+# Wait to complete boot
+sleep 20
+
 # Retrieve APT package sources
 sudo apt-get update -qq
 
